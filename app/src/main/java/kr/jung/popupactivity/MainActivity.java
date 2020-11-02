4568e3aee88458312d364847435140f78b80fdac
jung4337 <jungjyeeyoung@gmail.com>
2020-11-03 00:50:40 +0900

    팝업창을 띄우는 기능

---
+++ app/src/main/java/kr/jung/popupactivity/MainActivity.java
@@ -0,0 +1,37 @@
+package kr.jung.popupactivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView txtResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtResult = (TextView)findViewById(R.id.txtResult);
+    }
+
+    public void mOnPopupClick(View v){
+        Intent intent = new Intent(this, PopupActivity.class);
+        intent.putExtra("data", "Test Popup");
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                String result = data.getStringExtra("result");
+                txtResult.setText(result);
+            }
+        }
+    }
+}
